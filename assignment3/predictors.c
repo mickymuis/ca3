6a05dea3c5cfde7d6fa668877879e0caa538d8a5
Orson Peters <orsonpeters@gmail.com>
2014-11-17 12:10:53 +0100

    Finished cleanup, consistent code style, fixed warnings.

--- assignment3/predictors.c
+++ assignment3/predictors.c
@@ -9,104 +9,84 @@
 #define PUSH_FRONT(q, b) (q << 1) | (b ? 0x1 : 0x0)
 
 void pushFront_8( bitQueue8_t* q, bool b ) {
-    (*q) = PUSH_FRONT( (*q), b );
+    (*q) = PUSH_FRONT((*q), b);
 }
 
 void pushFront_16( bitQueue16_t* q, bool b ) {
-    (*q) = PUSH_FRONT( (*q), b );
+    (*q) = PUSH_FRONT((*q), b);
 }
 
 void pushFront_32( bitQueue32_t* q, bool b ) {
-    (*q) = PUSH_FRONT( (*q), b );
+    (*q) = PUSH_FRONT((*q), b);
 }
 
 void pushFront( bitQueue_t* q, uint16_t length, bool b ) {
-    (*q) = PUSH_FRONT( (*q), b );
+    (*q) = PUSH_FRONT((*q), b);
     (*q) &= (1 << length) -1;
   
-  // this is push-back
-  //  (*q) = ((*q) >> 1) | ( (b ? 0x1 : 0x0) << (length -1) );
+    // This is push-back:
+    // (*q) = ((*q) >> 1) | ((b ? 0x1 : 0x0) << (length -1));
 }
 
 
-/* Random prediction */
+// Random prediction.
 void random_predictor() {
-    /* Variable to store the prediction you predict for this branch. */
+    // Variable to store the prediction you predict for this branch.
     bool prediction = false;
 
-    /* Variable to store the the address of the branch. */
+    // Variable to store the the address of the branch.
     uint32_t addr = 0;
     
-    /* 
-     * Variable to store the actual branch result 
-     * (obtained from the predictor library)
-     */
+    // Variable to store the actual branch result (obtained from the predictor library).
     bool actual = false;
 
-    /* 
-     * Prediction loop, until tracefile is empty.
-     */
+    // Prediction loop, until tracefile is empty.
     while (predictor_getState() != DONE) {
-
-        /* Get the next branch address from the state machine. */
+        // Get the next branch address from the state machine.
         if (predictor_getNextBranch(&addr) != 0) {
             fprintf(stderr, "ERROR: \"predictor_getNextBranch()\" called in "\
                    "a state it shouldn't be called!\n");
         }
 
-        /* Do a random prediction */
+        // Do a random prediction.
         prediction = rand() & 1;
 
-        /* 
-         * Feed the prediction to the state machine, and get the actual
-         * result back.
-         */
+        // Feed the prediction to the state machine, and get the actual result back.
         if (predictor_predict(prediction, &actual) != 0) {
             fprintf(stderr, "ERROR: \"predictor_predict()\" called in "\
                     " a state it shouldn't be called\n");
         }
-        
     }
 }
 
 
-/* Predict always true (taken) or false */
+// Predict always true (taken) or false.
 void always_x(bool p) {
-    /* Variable to store the prediction you predict for this branch. */
+    // Variable to store the prediction you predict for this branch.
     bool prediction = p;
 
-    /* Variable to store the the address of the branch. */
+    // Variable to store the the address of the branch.
     uint32_t addr = 0;
     
-    /* 
-     * Variable to store the actual branch result 
-     * (obtained from the predictor library)
-     */
+    // Variable to store the actual branch result (obtained from the predictor library).
     bool actual = false;
 
-    /* 
-     * Prediction loop, until tracefile is empty.
-     */
+    // Prediction loop, until tracefile is empty.
     while (predictor_getState() != DONE) {
-
-        /* Get the next branch address from the state machine. */
+        // Get the next branch address from the state machine.
         if (predictor_getNextBranch(&addr) != 0) {
             fprintf(stderr, "ERROR: \"predictor_getNextBranch()\" called in "\
                    "a state it shouldn't be called!\n");
         }
 
-        /* Do a prediction */
+        // Do a prediction.
         prediction = p;
 
-        /* 
-         * Feed the prediction to the state machine, and get the actual
-         * result back.
-         */
+        // Feed the prediction to the state machine, and get the actual result back.
         if (predictor_predict(prediction, &actual) != 0) {
             fprintf(stderr, "ERROR: \"predictor_predict()\" called in "\
                     " a state it shouldn't be called\n");
         }
-        
     }
 }
 
@@ -141,64 +121,61 @@ void assignment_1_simple() {
 
 
 void assignment_2_GAg(int history) {
-    // The Branch History Register
+    // The Branch History Register.
     const int queueLength = history <= 64 ? history : 64;
-    bitQueue_t branchRegister =0; // this is the 'G' in Gag
+    bitQueue_t branchRegister = 0; // This is the 'G' in Gag.
              
-    // The Pattern History Table
-    const uint64_t k = 1 << queueLength; // n bit queue length, table has 2^n entries
-    counter_t patternTable[k]; // This is the 'g' in Gag
+    // The Pattern History Table.
+    const uint64_t k = 1 << queueLength; // n bit queue length, table has 2^n entries.
+    counter_t patternTable[k]; // This is the 'g' in Gag.
     
-    printf("GAg: Branch History Register length is %d bits, %d PHT entries\n", 
-            queueLength, k ); // May go away :-)
+    printf("GAg: Branch History Register length is %d bits, %" PRIu64 " PHT entries\n", 
+            queueLength, k); // May go away :-)
 
-    // Initialize the PHT
-    for( uint64_t i =0; i < k; i++ ) {
-        patternTable[i] =2; // 'Weak taken'
-    }
+    // Initialize the PHT.
+    for (uint64_t i = 0; i < k; i++) patternTable[i] = 2; // 'Weak taken'.
        
-    uint32_t addr =0;
+    uint32_t addr = 0;
     bool actual, prediction;
 
-    while( predictor_getState( ) != DONE ) {
-
-        if( predictor_getNextBranch( &addr ) )
-            fprintf( stderr, "ERROR: couldn't get next branch.\n" );
+    while (predictor_getState() != DONE) {
+        if (predictor_getNextBranch(&addr)) {
+            fprintf(stderr, "ERROR: couldn't get next branch.\n");
+        }
             
-        // Use the most recent pattern in the Branch Register 
-        // to lookup a prediction in the Pattern Table
+        // Use the most recent pattern in the Branch Register to lookup a prediction in the Pattern
+        // Table.
         prediction = patternTable[branchRegister] > 2;
 
-        if( predictor_predict( prediction, &actual ) )
-            fprintf( stderr, "ERROR: couldn't call predictor_predict( ).\n" );
+        if (predictor_predict( prediction, &actual)) {
+            fprintf(stderr, "ERROR: couldn't call predictor_predict().\n");
+        }
             
-        // Update the Pattern History Table to reflect the outcome
-        if( actual && ( patternTable[branchRegister] < 3 ) )
-            patternTable[branchRegister]++;
-        else if( !actual && ( patternTable[branchRegister] > 0 ) )
-            patternTable[branchRegister]--;
+        // Update the Pattern History Table to reflect the outcome.
+             if  (actual && (patternTable[branchRegister] < 3)) patternTable[branchRegister]++;
+        else if (!actual && (patternTable[branchRegister] > 0)) patternTable[branchRegister]--;
             
-        // Add the actual outcome to the current pattern in the Branch History Register
-        pushFront( &branchRegister, queueLength, actual );
+        // Add the actual outcome to the current pattern in the Branch History Register.
+        pushFront(&branchRegister, queueLength, actual);
     }
 }
 
-/* Implement assignment 3 here */
+// Implement assignment 3 here.
 void assignment_3_SAs(int history, int n_sets) {
-  always_x(false);
+    always_x(false);
 }
 
-/* Assignment 4: Change these parameters to your needs */
+// Assignment 4: Change these parameters to your needs.
 void assignment_4_your_own(int k, int n) {
-  always_x(false);
+    always_x(false);
 }
 
-/* Bonus: Change these parameters to your needs */
+// Bonus: Change these parameters to your needs.
 void bonus_1() {
-  always_x(false);
+    always_x(false);
 }
 
-/* Bonus: Change these parameters to your needs */
+// Bonus: Change these parameters to your needs.
 void bonus_2() {
-  always_x(false);
+    always_x(false);
 }
